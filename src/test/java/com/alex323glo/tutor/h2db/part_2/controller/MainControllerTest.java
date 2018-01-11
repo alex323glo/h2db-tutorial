f706186e2f28c30079ed8af7fb562a8ee129aecb
alex323glo <alex323glo@gmail.com>
2018-01-11 18:32:57 +0200

    Added Unit tests for MainController (will be completed in next updates).

---
+++ src/test/java/com/alex323glo/tutor/h2db/part_2/controller/MainControllerTest.java
@@ -0,0 +1,39 @@
+package com.alex323glo.tutor.h2db.part_2.controller;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void register() throws Exception {
+    }
+
+    @Test
+    public void login() throws Exception {
+    }
+
+    @Test
+    public void logout() throws Exception {
+    }
+
+    @Test
+    public void getUserInfoAsJSON() throws Exception {
+    }
+
+    @Test
+    public void getAllUsersAsJSON() throws Exception {
+    }
+
+}
