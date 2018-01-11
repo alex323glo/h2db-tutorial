338094676812aa6c5baa50db1674b1736d16315f
alex323glo <alex323glo@gmail.com>
2018-01-11 14:28:25 +0200

    Added MainController implementation (will be completed in nxt updates).

--- src/main/java/com/alex323glo/tutor/h2db/part_2/controller/MainControllerImpl.java
+++ src/main/java/com/alex323glo/tutor/h2db/part_2/controller/MainControllerImpl.java
@@ -1,14 +1,32 @@
 package com.alex323glo.tutor.h2db.part_2.controller;
 
+import com.alex323glo.tutor.h2db.part_2.dao.DAO;
 import com.alex323glo.tutor.h2db.part_2.exception.AppException;
+import com.alex323glo.tutor.h2db.part_2.model.user.User;
 
 /**
- * Main Application controller interface.
+ * Main Application controller implementation.
  *
  * @author alex323glo
  * @version 1.0
+ *
+ * @see MainController
  */
-public interface MainController {
+public class MainControllerImpl implements MainController {
+
+    private DAO<String, User> userDAO;
+
+    public MainControllerImpl(DAO<String, User> userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    public DAO<String, User> getUserDAO() {
+        return userDAO;
+    }
+
+    public void setUserDAO(DAO<String, User> userDAO) {
+        this.userDAO = userDAO;
+    }
 
     /**
      * Registers new user in system.
@@ -17,11 +35,14 @@ public interface MainController {
      * @param password password for new user.
      * @return String access token of registered (and automatically authorised) user.
      * @throws AppException if params don't pass validation or service has some problems
-     * with execution of this part of logic (check cause of thrown AppException).
-     *
+     *                      with execution of this part of logic (check cause of thrown AppException).
      * @see AppException
      */
-    String register(String username, String password) throws AppException;
+    @Override
+    public String register(String username, String password) throws AppException {
+        // TODO finish implementation
+        return null;
+    }
 
     /**
      * Authorises registered user in system.
@@ -30,20 +51,26 @@ public interface MainController {
      * @param password password of this existent user.
      * @return String access token of authorised user.
      * @throws AppException if params don't pass validation or service has some problems
-     * with execution of this part of logic (check cause of thrown AppException).
-     *
+     *                      with execution of this part of logic (check cause of thrown AppException).
      * @see AppException
      */
-    String login(String username, String password) throws AppException;
+    @Override
+    public String login(String username, String password) throws AppException {
+        // TODO finish implementation
+        return null;
+    }
 
     /**
      * Removes access token from list (in system) of authorised users.
      *
      * @param accessToken token, which will be removed from system.
      * @throws AppException if params don't pass validation or service has some problems
-     * with execution of this part of logic (check cause of thrown AppException).
+     *                      with execution of this part of logic (check cause of thrown AppException).
      */
-    void logout(String accessToken) throws AppException;
+    @Override
+    public void logout(String accessToken) throws AppException {
+        // TODO finish implementation
+    }
 
     /**
      * Returns JSON object of personal single user data record (accessed by unique USER access token)
@@ -53,9 +80,13 @@ public interface MainController {
      *                    record of user data, linked to this access token after authorisation..
      * @return JSON object, represented as String.
      * @throws AppException if params don't pass validation or service has some problems
-     * with execution of this part of logic (check cause of thrown AppException).
+     *                      with execution of this part of logic (check cause of thrown AppException).
      */
-    String getUserInfoAsJSON(String accessToken) throws AppException;
+    @Override
+    public String getUserInfoAsJSON(String accessToken) throws AppException {
+        // TODO finish implementation
+        return null;
+    }
 
     /**
      * Returns JSON object of personal all user data records (accessed by unique ROOT access token)
@@ -65,8 +96,11 @@ public interface MainController {
      *                    of user data (gives root access).
      * @return JSON object, represented as String.
      * @throws AppException if params don't pass validation or service has some problems
-     * with execution of this part of logic (check cause of thrown AppException).
+     *                      with execution of this part of logic (check cause of thrown AppException).
      */
-    String getAllUsersAsJSON(String accessToken) throws AppException;
-
+    @Override
+    public String getAllUsersAsJSON(String accessToken) throws AppException {
+        // TODO finish implementation
+        return null;
+    }
 }
